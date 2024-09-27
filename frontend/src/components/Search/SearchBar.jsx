220573da09e14e513d2857a31157e5fbf6b31b81
Nenad Blagov <nbl@nexyo.ag>
2024-09-27 16:22:25 +0200

    filters dropdown mockup

--- frontend/src/components/Search/SearchBar.jsx
+++ frontend/src/components/Search/SearchBar.jsx
@@ -27,22 +27,49 @@ function SearchBar() {
                 <Form.Control
                     onChange={handleChange}
                     type="search"
+                    id={styles.searchBar}
                     placeholder="Search datasets..."
                     value={searchTerm}
                 />
                 <InputGroup.Text>
-                    <div onClick={toggleFiltersDropdown} className='rounded-lg hover pointer p-1'>
-                        <Filter className='me-2' /> <span className='medium'>Filters</span>
-                    </div>
+                    <Dropdown show={filtersDropdopwnVisible}>
+                        <div onClick={toggleFiltersDropdown} className='rounded-lg hover pointer p-1'>
+                            <Filter className='me-2' /> <span className='medium'>Filters</span>
+                        </div>
+                        <Dropdown.Menu className='border-0 shadow-sm' style={{ top: 0, transform: 'translate(-65%, 50px)' }}>
+                            <div className='py-3 px-3'>
+                                <div className='mb-3'>
+                                    <label className='mb-1'>Tags</label>
+                                    <input type="text" className='form-control' placeholder='keywords' />
+                                </div>
+                                <div className='mb-3'>
+                                    <label className='mb-1'>Dataroom</label>
+                                    <div className='d-flex'>
+                                        <button className='btn btn-basic rounded-lg me-2'>MDS</button>
+                                        <button className='btn btn-basic rounded-lg me-2'>Medien</button>
+                                        <button className='btn btn-basic rounded-lg me-2'>NAP</button>
+                                        <button className='btn btn-basic rounded-lg me-2'>Other</button>
+                                    </div>
+                                </div>
+                                <div className='mb-3'>
+                                    <label className='mb-1'>File Size</label> <br />
+                                    <input type="range" min={0} max={5} className='w-100' />
+                                </div>
+                                <div className='mb-3'>
+                                    <label className='mb-1'>Data Structure</label>
+                                    <div className='d-flex'>
+                                        <button className='btn btn-basic rounded-lg me-2'>Video & Image</button>
+                                        <button className='btn btn-basic rounded-lg me-2'>Audio</button>
+                                        <button className='btn btn-basic rounded-lg me-2'>Documents</button>
+                                        <button className='btn btn-basic rounded-lg me-2'>Graphs</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
                 </InputGroup.Text>
 
-                <Dropdown show={filtersDropdopwnVisible} align="end">
-                    <Dropdown.Menu align="end">
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
 
             </InputGroup>
             <div className='ps-3'>
