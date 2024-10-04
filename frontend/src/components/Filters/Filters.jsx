fd6c54ac403f27afab9077e33370ef58aa20cf9d
Nenad Blagov <nbl@nexyo.ag>
2024-10-04 03:01:32 +0200

    custom checkbox

--- frontend/src/components/Filters/Filters.jsx
+++ frontend/src/components/Filters/Filters.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
-import { filters } from '../../utils/filter_utils'
-import { FormGroup } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { filters } from '../../utils/filter_utils';
+import { FormGroup } from 'react-bootstrap';
+import CustomCheckbox from 'react-custom-checkbox';
+import { Check, Dot } from 'react-bootstrap-icons';
 
 function Filters() {
+    const [checkedOptions, setCheckedOptions] = useState({});
+
+    const handleCheckboxChange = (option) => {
+        setCheckedOptions((prev) => ({
+            ...prev,
+            [option]: !prev[option],
+        }));
+    };
 
     return (
         <div className='d-flex flex-column'>
             {filters.map((filter) => (
-                <FormGroup>
+                <FormGroup key={filter.name}>
                     <label className='mt-4 mb-2 small text-uppercase'>{filter.label}</label>
-                    {filter.type == "checkbox" ? (
+                    {filter.type === "checkbox" ? (
                         filter.options.map((option) => (
-                            <div className='d-flex align-items-center'>
-                                <input type="checkbox" className='text-muted' name={filter.name} checked />
-                                <label className='ps-2 text-muted'>{option}</label>
+                            <div className='d-flex align-items-center' key={option}>
+                                <CustomCheckbox
+                                    checked={!!checkedOptions[option]}
+                                    label={option}
+                                    onChange={() => handleCheckboxChange(option)}
+                                    borderColor="var(--color-primary)"
+                                    icon={<Check className='txt-primary' />}
+                                />
                             </div>
                         ))
-                    ) : filter.type == "range" ? (
+                    ) : filter.type === "range" ? (
                         <div className='d-flex'>
                             <input type="range" name={filter.name} />
                         </div>
-                    ) : ''
-                    }
+                    ) : null}
                 </FormGroup>
             ))}
         </div>
-    )
+    );
 }
 
-export default Filters
+export default Filters;
