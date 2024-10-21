7c07c97cb87153b2556b8af2e5dd2e715c02a1a5
Nenad Blagov <nbl@nexyo.ag>
2024-10-21 16:39:33 +0200

    zoom in option

--- frontend/src/modules/common/components/Header/ImageView/ImageView.jsx
+++ frontend/src/modules/common/components/Header/ImageView/ImageView.jsx
@@ -1,22 +1,58 @@
 import React, { useState } from 'react';
-import Zoom from 'react-medium-image-zoom';
-import 'react-medium-image-zoom/dist/styles.css';
 import styles from './ImageView.module.css';
+import { ZoomIn, ZoomOut } from 'react-bootstrap-icons';
 
 function ImageView({ url }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [zoom, setZoom] = useState(50);
+
+    const openImage = () => setIsOpen(true);
+    const closeImage = () => {
+        setIsOpen(false);
+        setZoom(50);
+    };
+
+    const zoomIn = () => setZoom((prevZoom) => Math.min(prevZoom + 5, 100));
+    const zoomOut = () => setZoom((prevZoom) => Math.max(prevZoom - 5, 50));
+
     return (
         <>
-            <div className='position-relative'>
-                <Zoom>
-                    <img
-                        src={url}
-                        alt="image"
-                        className={styles.image}
-                        loading="lazy"
-                    />
-                </Zoom>
+            <img
+                src={url}
+                alt="image"
+                className={styles.image}
+                loading="lazy"
+                onClick={openImage}
+                style={{ cursor: 'pointer' }}
+            />
 
-            </div>
+            {isOpen && (
+                <div className={styles.modal}>
+                    <div className={styles.modalContent}>
+                        <button className={styles.closeButton} onClick={closeImage}>×</button>
+
+                        <div className={styles.zoomControls}>
+                            <button className="btn btn-primary mx-2" onClick={zoomIn}>
+                                <ZoomIn />
+                            </button>
+                            <button className="btn btn-primary mx-2" onClick={zoomOut}>
+                                <ZoomOut />
+                            </button>
+                        </div>
+
+                        <div
+                            className={styles.imageContainer}
+                        >
+                            <img
+                                src={url}
+                                alt="Zoomed"
+                                className={styles.zoomableImage}
+                                style={{ width: `${zoom}%`, height: 'auto' }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
