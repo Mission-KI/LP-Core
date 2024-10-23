11560bea76fa68b0381f715b290b0632e3a3f17f
Nenad Blagov <nbl@nexyo.ag>
2024-10-23 20:21:15 +0200

    test commit

--- frontend/src/modules/dataset/pages/Details/Details.jsx
+++ frontend/src/modules/dataset/pages/Details/Details.jsx
@@ -299,6 +299,7 @@ function Details() {
                     ATTRIBUTE
                     <br />
                     CONSISTENCY
+                    
                   </span>
                 }
                 className={styles.tab}
