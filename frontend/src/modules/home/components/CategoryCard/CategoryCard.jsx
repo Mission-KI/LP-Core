7d2fd340c763469f75cdfff8428a8af372093d4d
Nenad Blagov <nbl@nexyo.ag>
2024-11-09 13:37:37 +0100

    updates

--- frontend/src/modules/home/components/CategoryCard/CategoryCard.jsx
+++ frontend/src/modules/home/components/CategoryCard/CategoryCard.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import { Database, Files } from 'react-bootstrap-icons';
 import Card from 'react-bootstrap/Card';
 
 const CategoryCard = ({ category }) => {
     return (
         <div className="col-md-4 mb-4">
             <Card className='h-100'>
-                {category.image ? (
-                    <Card.Img variant="top" src={category.image} />
-                ) : (
-                    <Card.Img variant="top" src="path/to/default-image.jpg" alt="Default Category" />
-                )}
+                <Card.Img variant="top" src={category.image} />
                 <Card.Body>
                     <Card.Title>{category.name}</Card.Title>
+                    <Card.Text className='medium txt-lighter'>
+                        Some quick example text to build on the card.
+                    </Card.Text>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
+                        <div className="row mt-3">
+                            <div className="col-6">
+                                <span className='small d-flex align-items-center'>
+                                    <Database className='me-1' /> 25 Data sources
+                                </span>
+                            </div>
+                            <div className="col-6">
+                                <span className='small d-flex align-items-center'>
+                                    <Files className='me-1' /> 5.275.180 Data Assets
+                                </span>
+                            </div>
+                        </div>
                     </Card.Text>
                 </Card.Body>
             </Card>
