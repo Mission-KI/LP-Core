af4cce8fa0b68a63b2cec960fa438870bed8f0b6
Nenad Blagov <nbl@nexyo.ag>
2024-11-21 13:36:28 +0100

    minor update

--- src/modules/common/components/TitleSection.jsx
+++ src/modules/common/components/TitleSection.jsx
@@ -10,7 +10,7 @@ const TitleSection = () => {
         <div className='d-flex justify-content-between mb-5'>
             <div className='d-flex align-items-center'>
                 <a href="/" className='text-decoration-none h2 mb-0' style={{ width: 'fit-content' }}>{t('page.title')}</a>
-                <span className='badge badge-primary bg-danger ms-2'>Alpha</span>
+                <span className='badge badge-primary bg-danger ms-2' style={{ fontSize: 15 }}>Alpha</span>
             </div>
             <LanguageSelector />
         </div>
