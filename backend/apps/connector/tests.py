05b6e213581a06e0654c60989ea2b647ed696ddf
Kai Buschulte <kb@beebucket.de>
2025-01-29 16:46:42 +0100

    Validate EDP payload with schema

--- backend/apps/connector/tests.py
+++ backend/apps/connector/tests.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from io import BytesIO, StringIO
 from typing import Dict
 from zipfile import ZipFile
@@ -5,6 +6,13 @@ from zipfile import ZipFile
 import pytest
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.urls import reverse
+from extended_dataset_profile import SchemaVersion
+from extended_dataset_profile.models.v0.edp import (
+    DataSpace,
+    ExtendedDatasetProfile,
+    License,
+    Publisher,
+)
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.test import APIClient
@@ -34,6 +42,23 @@ def create_zip(file_list: Dict[str, str]):
     zip_buf.seek(0)
     return InMemoryUploadedFile(
         zip_buf, name="test_file.zip", content_type="application/zip", field_name=None, size=None, charset=None
+    )
+
+
+@pytest.fixture
+def mini_edp():
+    return ExtendedDatasetProfile(
+        schema_version=SchemaVersion.V0,
+        volume=4307,
+        dataTypes=set(),
+        assetId="did:op:ACce67694eD2848dd683c651Dab7Af823b7dd123",
+        name="Sample asset",
+        url="https://portal.pontus-x.eu/asset/did:op:ACce67694eD2848dd683c651Dab7Af823b7dd123",
+        dataSpace=DataSpace(name="Pontus-X", url="https://portal.pontus-x.eu"),
+        publisher=Publisher(name="OPF", url=None),
+        publishDate=datetime(year=2026, month=12, day=1),
+        license=License(name=None, url="https://market.oceanprotocol.com/terms"),
+        freely_available=False,
     )
 
 
@@ -76,12 +101,27 @@ def test_create_edp_file_zip_missing_json(client: APIClient, create_edp_url: str
     assert response.data == {"detail": "No JSON file found in the ZIP archive."}
 
 
-def test_create_edp_file_zip_success(client: APIClient, create_edp_url: str):
+def test_create_edp_file_zip_validation_error(client: APIClient, create_edp_url: str):
     response = client.post(
         create_edp_url,
-        {"file": create_zip({"dummy_edp.json": '{"hello": "world"}', "some_other_file_will_be_ignored.png": ""})},
+        {"file": create_zip({"dummy_edp.json": '{"volume": "world"}', "some_other_file_will_be_ignored.png": ""})},
+        format="multipart",
+    )
+    assert isinstance(response, Response)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert isinstance(response.data, dict)
+    assert "detail" in response.data
+    assert response.data["detail"].startswith("10 validation errors for ExtendedDatasetProfile"), response.data[
+        "detail"
+    ]
+
+
+def test_create_edp_file_zip_success(client: APIClient, create_edp_url: str, mini_edp: ExtendedDatasetProfile):
+    response = client.post(
+        create_edp_url,
+        {"file": create_zip({"dummy_edp.json": mini_edp.model_dump_json(), "some_other_file_will_be_ignored.png": ""})},
         format="multipart",
     )
     assert isinstance(response, Response)
     assert response.status_code == status.HTTP_200_OK
-    assert response.data == {"message": "JSON file processed successfully", "json_content": {"hello": "world"}}
+    assert response.data == {"message": "JSON file processed successfully", "json_content": mini_edp.model_dump()}
--- backend/apps/connector/views.py
+++ backend/apps/connector/views.py
@@ -1,11 +1,12 @@
-import json
 import zipfile
 from logging import getLogger
 
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from extended_dataset_profile import schema_versions
+from pydantic import ValidationError as PydanticValidationError
 from rest_framework import status
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError as DRFValidationError
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -41,7 +42,7 @@ class EDPView(APIView):
         logger.info(f"Received {file.name}")
 
         if not file.name.endswith(".zip"):
-            raise ValidationError({"detail": "Only ZIP files are accepted."})
+            raise DRFValidationError({"detail": "Only ZIP files are accepted."})
 
         try:
             with zipfile.ZipFile(file, "r") as z:
@@ -60,20 +61,31 @@ class EDPView(APIView):
                 logger.info(f"found JSON file {json_filename}")
 
                 with z.open(json_filename) as json_file:
-                    json_content = json.load(json_file)
-
-                return Response(
+                    for schema in schema_versions.values():
+                        try:
+                            json_content = schema.model_validate_json(json_file.read())
+                            return Response(
+                                {
+                                    "message": "JSON file processed successfully",
+                                    "json_content": json_content.model_dump(),
+                                },
+                                status=status.HTTP_200_OK,
+                            )
+                        except PydanticValidationError as e:
+                            raise DRFValidationError({"detail": str(e)})
+                return DRFValidationError(
                     {
-                        "message": "JSON file processed successfully",
-                        "json_content": json_content,
+                        "detail": "JSON not matching any known EDP JSON schema. Supported versions:"
+                        f" {', '.join([v.value for v in schema_versions.keys()])}"
                     },
-                    status=status.HTTP_200_OK,
                 )
 
         except zipfile.BadZipFile:
-            raise ValidationError({"detail": "Uploaded file is not a valid ZIP archive."})
+            raise DRFValidationError({"detail": "Uploaded file is not a valid ZIP archive."})
+        except DRFValidationError as e:
+            raise e
         except Exception as e:
             return Response(
-                {"detail": f"An error occurred: {str(e)}"},
+                {"detail": f"An unknown error occurred: {str(e)}"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
