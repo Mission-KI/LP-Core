9f7489ab6757a9f40ea5c2a8c365616c5cd76b6f
Nenad Blagov <nbl@nexyo.ag>
2025-01-29 09:38:52 +0100

    skeleton loading animation

---
+++ frontend/src/modules/common/animations/SkeletonAnimation.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function SkeletonLoader() {
+    return (
+        <div className='col-md-6'>
+            <div className="rounded-0 pb-1 mt-3 mb-2">
+                <div className="pt-3 pb-2">
+                    <div className="bone bone-text-short mb-3"></div>
+                </div>
+                <div>
+                    <div className="bone bone-description"></div>
+                </div>
+            </div>
+            <div className="rounded-0 pb-1 mt-3 mb-2">
+                <div className="pt-3 pb-2">
+                    <div className="bone bone-text-short mb-3"></div>
+                </div>
+                <div>
+                    <div className="bone bone-description"></div>
+                </div>
+            </div>
+            <div className="rounded-0 pb-1 mt-3 mb-2">
+                <div className="pt-3 pb-2">
+                    <div className="bone bone-text-short mb-3"></div>
+                </div>
+                <div>
+                    <div className="bone bone-description"></div>
+                </div>
+            </div>
+            <div className="rounded-0 pb-1 mt-3 mb-2">
+                <div className="pt-3 pb-2">
+                    <div className="bone bone-text-short mb-3"></div>
+                </div>
+                <div>
+                    <div className="bone bone-description"></div>
+                </div>
+            </div>
+            <div className="rounded-0 pb-1 mt-3 mb-2">
+                <div className="pt-3 pb-2">
+                    <div className="bone bone-text-short mb-3"></div>
+                </div>
+                <div>
+                    <div className="bone bone-description"></div>
+                </div>
+            </div>
+            <div className="rounded-0 pb-1 mt-3 mb-2">
+                <div className="pt-3 pb-2">
+                    <div className="bone bone-text-short mb-3"></div>
+                </div>
+                <div>
+                    <div className="bone bone-description"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SkeletonLoader
