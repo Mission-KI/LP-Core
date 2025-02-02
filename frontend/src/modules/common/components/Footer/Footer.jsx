10f88ae989c8d5705b8ee85cc37210d7ce8d3465
Nenad Blagov <nbl@nexyo.ag>
2025-02-02 18:01:19 +0100

    minor style changes

--- frontend/src/modules/common/components/Footer/Footer.jsx
+++ frontend/src/modules/common/components/Footer/Footer.jsx
@@ -10,16 +10,19 @@ const Footer = () => {
     const { t } = useTranslation();
 
     return (
-        <div className={styles.footer}>
+        <div className={`${styles.footer} shadow`}>
             <div className="container pt-4 pb-4" style={{ maxWidth: 1100 }}>
-                <div className='d-flex flex-wrap justify-content-between align-items-center w-100' style={{ maxWidth: 650 }}>
-                    <div className='pe-2'>
-                        <img src={icon} className='rounded' style={{ height: 35 }} alt="" />
+                <div className='d-flex flex-wrap justify-content-between align-items-center'>
+                    <div className='d-flex flex-wrap justify-content-between align-items-center w-100' style={{ maxWidth: 600 }}>
+                        <div className='pe-2'>
+                            <img src={icon} className='rounded' style={{ height: 35 }} alt="" />
+                        </div>
+                        <a href={`${landingUrl}/privacy-policy`} className={styles.footerLink}>{t('footer.privacyPolicy')}</a>
+                        <a href={`${landingUrl}/use-policy`} className={styles.footerLink}>{t('footer.usePolicy')}</a>
+                        <a href={`${landingUrl}/imprint`} className={styles.footerLink}>{t('footer.imprint')}</a>
+                        <a href='/help' className={styles.footerLink}>Help</a>
                     </div>
-                    <span className={`${styles.footerLink}`}><span className='txt-primary'>{t('footer.c')}</span></span>
-                    <a href={`${landingUrl}/privacy-policy`} className={styles.footerLink}>{t('footer.privacyPolicy')}</a>
-                    <a href={`${landingUrl}/use-policy`} className={styles.footerLink}>{t('footer.usePolicy')}</a>
-                    <a href={`${landingUrl}/imprint`} className={styles.footerLink}>{t('footer.imprint')}</a>
+                    <span className={`${styles.footerLink}`}><span className='txt-primary h6 mb-0'>{t('footer.c')}</span></span>
                 </div>
             </div>
         </div>
