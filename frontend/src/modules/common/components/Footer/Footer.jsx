b37a4adef3edd323736a21def760c473cc8b7680
Nenad Blagov <nbl@nexyo.ag>
2025-02-02 10:50:00 +0100

    footer improvements

--- frontend/src/modules/common/components/Footer/Footer.jsx
+++ frontend/src/modules/common/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import styles from './Footer.module.css'
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { landingUrl } from '../../api/config';
+import icon from '../../assets/img/brand/icon.png'
 
 const Footer = () => {
 
@@ -11,11 +12,14 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
             <div className="container pt-4 pb-4" style={{ maxWidth: 1100 }}>
-                <div className='d-flex w-100'>
+                <div className='d-flex flex-wrap justify-content-between align-items-center w-100' style={{ maxWidth: 650 }}>
+                    <div className='pe-2'>
+                        <img src={icon} className='rounded' style={{ height: 35 }} alt="" />
+                    </div>
                     <span className={`${styles.footerLink}`}><span className='txt-primary'>{t('footer.c')}</span></span>
-                    <a href={`${landingUrl}/imprint`} className={styles.footerLink}>{t('footer.imprint')}</a>
                     <a href={`${landingUrl}/privacy-policy`} className={styles.footerLink}>{t('footer.privacyPolicy')}</a>
                     <a href={`${landingUrl}/use-policy`} className={styles.footerLink}>{t('footer.usePolicy')}</a>
+                    <a href={`${landingUrl}/imprint`} className={styles.footerLink}>{t('footer.imprint')}</a>
                 </div>
             </div>
         </div>
