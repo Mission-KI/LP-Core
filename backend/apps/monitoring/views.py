82b44e785900450e631ec0c3ca4e40c1a663db1e
Kai Buschulte <kb@beebucket.de>
2025-03-05 11:51:40 +0100

    restrict access to monitoring/log-edp-download

--- backend/apps/monitoring/views.py
+++ backend/apps/monitoring/views.py
@@ -1,11 +1,28 @@
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from rest_framework import permissions
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 
 from .models import EventLog
 from .utils import create_log
 
 
+class HasAddEventLogPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        content_type = ContentType.objects.get_for_model(EventLog)
+        permission = Permission.objects.get(
+            codename="add_eventlog",  # or change_eventlog, delete_eventlog, add_eventlog.
+            content_type=content_type,
+        )
+        full_permission_string = f"{EventLog._meta.app_label}.{permission.codename}"
+        return request.user.has_perm(full_permission_string)
+
+
 @api_view(["POST"])
+@permission_classes([permissions.IsAuthenticated, HasAddEventLogPermission])
+@authentication_classes([BasicAuthentication])
 def log_edp_download(request):
     create_log(request.get_full_path(), "edp download", EventLog.STATUS_SUCCESS)
     return Response({"message": "success"})
