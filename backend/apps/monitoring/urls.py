e40f549ce8d48d6ce4761a49579dac7743e0d9c6
Nenad Blagov <nbl@nexyo.ag>
2025-03-15 18:00:55 +0100

    changed analytics url

--- backend/apps/monitoring/urls.py
+++ backend/apps/monitoring/urls.py
@@ -4,6 +4,6 @@ from . import views
 from .views import MonitoringAnalyticsView
 
 urlpatterns = [
-    path('monitoring-analytics/', MonitoringAnalyticsView.as_view(), name='monitoring-analytics'),
+    path('analytics/', MonitoringAnalyticsView.as_view(), name='analytics'),
     path("log-edp-download/", views.log_edp_download, name="log_edp_download"),
 ]
