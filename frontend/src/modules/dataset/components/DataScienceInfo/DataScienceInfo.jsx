5be1e0cb8124ed7e26ab53505adb4b9bc9af496b
Nenad Blagov <nbl@nexyo.ag>
2025-03-27 03:57:51 +0100

    add numeric correlation summary attribute

--- frontend/src/modules/dataset/components/DataScienceInfo/DataScienceInfo.jsx
+++ frontend/src/modules/dataset/components/DataScienceInfo/DataScienceInfo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { filesize } from "filesize";
 import { useTranslation } from "react-i18next";
-import { calculateAttributeIntegrity, calculateDataTypesAttribute, calculateTemporalCover, getStringValueDistributionOverview, getTopNumericDistributions, getUniqueNumericDistributions } from "../utils/calculations";
+import { calculateAttributeIntegrity, calculateDataTypesAttribute, calculateTemporalCover, getStringValueDistributionOverview, getTopNumericDistributions, getUniqueNumericDistributions } from "../../utils/calculations";
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { QuestionCircle } from 'react-bootstrap-icons';
 import { useNavigate } from 'react-router'
-import EdpStructure from './EdpStructure/EdpStructure'
-import { getEdpLanguagesList } from '../utils/edp_utils';
+import EdpStructure from '../EdpStructure/EdpStructure'
+import GeneralEdpScienceOverfiew from './GeneralEdpScienceOverfiew';
 
 const DataScienceInfo = ({ datasetDetails }) => {
 
@@ -24,48 +24,9 @@ const DataScienceInfo = ({ datasetDetails }) => {
     return (
         <div>
             <div className="row w-100">
-                <div className="col-6">
-                    <p className="small mb-1 fw-500 text-uppercase">{t("dataset.structure")}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1">Text (CSV)</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1 fw-500 text-uppercase">{t("dataset.volume")}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1">{filesize(datasetDetails?._source?.volume)}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1 fw-500 text-uppercase">{t("dataset.compression")}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1">{datasetDetails?._source?.compression ?? "None"}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1 fw-500 text-uppercase">{t("dataset.languages")}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1">{getEdpLanguagesList(datasetDetails)}</p>
-                </div>
-                <div className="col-6 mt-3">
-                    <p className="small mb-1 fw-500 text-uppercase">{t("dataset.transferType")}</p>
-                </div>
-                <div className="col-6 mt-3">
-                    <p className="small mb-1">{datasetDetails?._source?.transferTypeFlag ?? "unknown"}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1 fw-500 text-uppercase">{t("dataset.immutability")}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1">{datasetDetails?._source?.immutabilityFlag ?? "unknown"}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1 fw-500 text-uppercase">{t("dataset.dataTypes")}</p>
-                </div>
-                <div className="col-6">
-                    <p className="small mb-1">{calculateDataTypesAttribute(datasetDetails)}</p>
-                </div>
+                <GeneralEdpScienceOverfiew
+                    datasetDetails={datasetDetails}
+                />
                 <div className="col-6 mt-3">
                     <p className="small mb-1 fw-500 text-uppercase">{t("dataset.temporalCover")}</p>
                 </div>
