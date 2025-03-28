79478efdbac24a6681d683ac78ded9fd632c792e
Nenad Blagov <nbl@nexyo.ag>
2025-03-28 09:57:17 +0100

    download schema button

--- frontend/src/modules/dataset/components/DatasetActions.jsx
+++ frontend/src/modules/dataset/components/DatasetActions.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import { notifyEdpDownloadEvent } from "../api/dataset";
-import { imageBasePath } from "../../common/api/config";
+import { apiUrl, imageBasePath } from "../../common/api/config";
 
 const DatasetActions = ({ datasetDetails }) => {
 
@@ -49,11 +49,11 @@ const DatasetActions = ({ datasetDetails }) => {
                         </Link>
                     </div>
                 )}
-                <div className='pe-2 pt-1'>
-                    <span className='btn-hover px-2 py-2 txt-primary fw-500 pointer small d-flex align-items-center'>
+                <form method="GET" action={apiUrl + '/connector/edp/schema/'} className='pe-2 pt-1'>
+                    <button type="submit" className='btn btn-hover px-2 py-2 txt-primary fw-500 pointer small d-flex align-items-center'>
                         <Download className='me-1' /> {t('header.schemaJson')}
-                    </span>
-                </div>
+                    </button>
+                </form>
                 <div className='pe-2 pt-1'>
                     <a href={reportDownloadUrl} className='btn-hover px-2 py-2 txt-primary fw-500 pointer small d-flex align-items-center'>
                         <Download className='me-1' /> {t('header.reportPdf')}
