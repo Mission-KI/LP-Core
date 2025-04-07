d575e07c01333501956ccc0ce94ff54cede7114a
Nenad Blagov <nbl@nexyo.ag>
2025-04-07 23:47:53 +0200

    made base monitoring route redirect to dashboard

--- frontend/src/modules/monitoring/index.jsx
+++ frontend/src/modules/monitoring/index.jsx
@@ -1,6 +1,11 @@
+import { Navigate } from "react-router";
 import Dashboard from "./pages/Dashboard";
 
 const monitoringRoutes = [
+  {
+    path: '/monitoring',
+    element: <Navigate to="/monitoring/dashboard" />,
+  },
   {
     path: '/monitoring/dashboard',
     element: <Dashboard />,
