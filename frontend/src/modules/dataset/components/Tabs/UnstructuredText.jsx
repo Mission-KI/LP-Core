efe349dcc3e61c14f9bf9c0df1d34811b3b20d63
Nenad Blagovcanin <nbl@nexyo.ag>
2025-04-12 14:38:33 +0200

    integrated word clouds with react-d3-cloud

--- frontend/src/modules/dataset/components/Tabs/UnstructuredText.jsx
+++ frontend/src/modules/dataset/components/Tabs/UnstructuredText.jsx
@@ -1,7 +1,30 @@
-const UnstructuredText = ({ edp }) => {
+import WordCloud from "react-d3-cloud";
+
+const UnstructuredText = ({ dataset }) => {
+  const wordCloudData =
+    dataset?.wordCloud?.map(({ word, count }) => ({
+      text: word,
+      value: count,
+    })) || [];
+
+  const allSame = wordCloudData.every(
+    (w) => w.value === wordCloudData[0]?.value
+  );
+  const fontSize = allSame
+    ? () => 25
+    : (word) => Math.log2(word.value + 1) * 15;
+
   return (
-    <div>
-      <p>No data available for this table.</p>
+    <div style={{ width: "100%", height: "400px" }} className="border-lighter">
+      <WordCloud
+        data={wordCloudData}
+        fontSize={fontSize}
+        rotate={(word) => word.value % 360}
+        padding={20}
+        height={400}
+        spiral="rectangular"
+        random={() => 0.5}
+      />
     </div>
   );
 };
