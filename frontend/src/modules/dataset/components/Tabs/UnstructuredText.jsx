f98152121cf08fd400af25eb7d6ec039c2eb79f4
Nenad Blagovcanin <nbl@nexyo.ag>
2025-04-14 13:47:45 +0200

    added missing attributes to unstructured text tab

--- frontend/src/modules/dataset/components/Tabs/UnstructuredText.jsx
+++ frontend/src/modules/dataset/components/Tabs/UnstructuredText.jsx
@@ -1,6 +1,9 @@
 import WordCloud from "react-d3-cloud";
+import { useTranslation } from "react-i18next";
 
 const UnstructuredText = ({ dataset }) => {
+  const { t } = useTranslation();
+
   const wordCloudData =
     dataset?.wordCloud?.map(({ word, count }) => ({
       text: word,
@@ -15,7 +18,34 @@ const UnstructuredText = ({ dataset }) => {
     : (word) => Math.log2(word.value + 1) * 15;
 
   return (
-    <div style={{ width: "100%", height: "400px" }} className="border-lighter">
+    <div style={{ width: "100%" }}>
+      <div className="table-responsive">
+        <table
+          id="unstructuredTextDetailsTable"
+          className="table table-bordered table-hover"
+        >
+          <tbody>
+            <tr>
+              <td className="py-2">
+                <strong>{t("dataset.languages")}</strong>
+              </td>
+              <td className="txt-lighter">{dataset?.languages?.join(", ")}</td>
+            </tr>
+            <tr>
+              <td className="py-2">
+                <strong>{t("dataset.noOfLines")}</strong>
+              </td>
+              <td className="txt-lighter">{dataset?.lineCount}</td>
+            </tr>
+            <tr>
+              <td className="py-2">
+                <strong>{t("dataset.noOfWords")}</strong>
+              </td>
+              <td className="txt-lighter">{dataset?.wordCount}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <WordCloud
         data={wordCloudData}
         fontSize={fontSize}
