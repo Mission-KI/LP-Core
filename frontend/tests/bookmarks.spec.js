1ad829ae5f8d1ecc480aa748645aac67c8724ce6
Sinja Rada <sr@beebucket.de>
2025-04-17 14:27:22 +0200

    (DSE-900) added more tests

--- frontend/tests/bookmarks.spec.js
+++ frontend/tests/bookmarks.spec.js
@@ -1,19 +1,28 @@
 import { test, expect } from "@playwright/test";
 
-test("bookmarks first result and verifies it on the bookmarks page", async ({
+// Selectors and xPaths
+const resultsSelector = '[data-test-id="result-link"]';
+const bookmarkButtonSelector = '[data-test-id="bookmark-button"]';
+const quickViewToggleSelector = '[data-test-id="quick-view-toggle-button"]';
+const xPathStar =
+  "xpath=//a[contains(@data-test-id, 'result-link') and text()='{TITLE}']/../div/span";
+const xPathThreeDots =
+  "xpath=//a[contains(@data-test-id, 'result-link')]/../../../div[contains(@class,'mt-1 dropdown')]/div";
+const xPathAlert =
+  "xpath=//div[contains(@role, 'alert')]/div[text()='{MESSAGE}']";
+const xPathCloseAlert = "xpath=//div[contains(@role, 'alert')]/../button";
+const messageSaved = "Bookmark saved!";
+const messageRemoved = "Bookmark removed!";
+
+test("bookmarks first result via quick view and verifies it on the bookmarks page", async ({
   page,
   baseURL,
 }) => {
   await page.goto(`${baseURL}`);
 
-  // Selector for the first result and bookmark button
-  const firstResultSelector = '[data-test-id="result-link"]';
-  const bookmarkButtonSelector = '[data-test-id="bookmark-button"]';
-  const quickViewToggleSelector = '[data-test-id="quick-view-toggle-button"]';
-
   // Get the name of the first result item
   const firstResultName = await page
-    .locator(firstResultSelector)
+    .locator(resultsSelector)
     .first()
     .innerText();
 
@@ -23,6 +32,21 @@ test("bookmarks first result and verifies it on the bookmarks page", async ({
   // Click the bookmark button for the first result item
   await page.locator(bookmarkButtonSelector).first().click();
 
+  // Verify Message
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageSaved)),
+  ).toBeVisible();
+  await page.locator(xPathCloseAlert).click();
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageSaved)),
+  ).toHaveCount(0);
+  await expect(page.locator(bookmarkButtonSelector)).toHaveCount(0);
+
+  // Verify star appears
+  await expect(
+    page.locator(xPathStar.replace("{TITLE}", firstResultName)),
+  ).toBeVisible();
+
   // Navigate to the /bookmarks page
   await page.goto(`${baseURL}/bookmarks`);
 
@@ -31,4 +55,174 @@ test("bookmarks first result and verifies it on the bookmarks page", async ({
 
   // Verify that the bookmarked item appears on the /bookmarks page
   await expect(page.locator(bookmarkedItemSelector)).toBeVisible();
+});
+
+test("bookmarks second result via three dots menu and verifies it on the bookmarks page", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+
+  // Get the name of the second result item
+  const secondResultName = await page
+    .locator(resultsSelector)
+    .nth(1)
+    .innerText();
+
+  // Scroll down to ensure there will not be an overlay issue
+  await page.locator(resultsSelector).nth(2).scrollIntoViewIfNeeded();
+  await page.locator(resultsSelector).nth(1).scrollIntoViewIfNeeded();
+
+  // Bookmark via three dots menu
+  await page.locator(xPathThreeDots).nth(1).click();
+  await page.getByText("Bookmark").click();
+
+  // Verify Message
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageSaved)),
+  ).toBeVisible();
+  await page.locator(xPathCloseAlert).click();
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageSaved)),
+  ).toHaveCount(0);
+
+  // Verify stars appeared
+  await expect(
+    page.locator(xPathStar.replace("{TITLE}", secondResultName)),
+  ).toBeVisible();
+
+  // Navigate to the /bookmarks page
+  await page.goto(`${baseURL}/bookmarks`);
+
+  // Selector for bookmarked item name in the /bookmarks page
+  const bookmarkedItemSelector = `[data-test-id="result-link"]:has-text("${secondResultName}")`;
+
+  // Verify that the bookmarked item appears on the /bookmarks page
+  await expect(page.locator(bookmarkedItemSelector)).toBeVisible();
+});
+
+test("bookmarks third result details page and verifies it on the bookmarks page", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+
+  // Get the name of the third result item
+  const thirdResultName = await page
+    .locator(resultsSelector)
+    .nth(2)
+    .innerText();
+
+  // Scroll to item and click to open details page
+  await page.locator(resultsSelector).nth(2).scrollIntoViewIfNeeded();
+  await page.locator(resultsSelector).nth(2).click();
+
+  // Bookmark via details page
+  await expect(page.locator(bookmarkButtonSelector)).toHaveText("Bookmark");
+  await page.locator(bookmarkButtonSelector).click();
+
+  // Verify Message
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageSaved)),
+  ).toBeVisible();
+  await page.locator(xPathCloseAlert).click();
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageSaved)),
+  ).toHaveCount(0);
+
+  // Verify item is bookmarked
+  await expect(page.locator(bookmarkButtonSelector)).not.toBeVisible();
+  await expect(page.getByText("Remove Bookmark")).toBeVisible();
+
+  // Navigate to the /bookmarks page
+  await page.goto(`${baseURL}/bookmarks`);
+
+  // Selector for bookmarked item name in the /bookmarks page
+  const bookmarkedItemSelector = `[data-test-id="result-link"]:has-text("${thirdResultName}")`;
+
+  // Verify that the bookmarked item appears on the /bookmarks page
+  await expect(page.locator(bookmarkedItemSelector)).toBeVisible();
+});
+
+test("removes bookmarked entry from bookmarks page via quick view", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+
+  // Set a bookmark first
+  const fourthResultName = await page
+    .locator(resultsSelector)
+    .nth(3)
+    .innerText();
+  await page.locator(resultsSelector).nth(3).scrollIntoViewIfNeeded();
+  await page.locator(quickViewToggleSelector).nth(3).click();
+  await page.locator(bookmarkButtonSelector).first().click();
+
+  // Navigate to the /bookmarks page
+  await page.goto(`${baseURL}/bookmarks`);
+
+  // Selector for bookmarked item name in the /bookmarks page
+  const bookmarkedItemSelector = `[data-test-id="result-link"]:has-text("${fourthResultName}")`;
+
+  // Verify that the bookmarked item appears on the /bookmarks page
+  await expect(page.locator(bookmarkedItemSelector)).toBeVisible();
+
+  // Click the quick view toggle button to reveal options including the bookmark button
+  await page.locator(quickViewToggleSelector).first().click();
+  await expect(page.locator(bookmarkButtonSelector).first()).not.toBeVisible();
+  await page.getByText("Remove Bookmark").click();
+
+  // Verify Message
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageRemoved)),
+  ).toBeVisible();
+  await page.locator(xPathCloseAlert).click();
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageRemoved)),
+  ).toHaveCount(0);
+
+  // Verify the bookmarked item disappeared
+  await expect(page.locator(bookmarkedItemSelector)).not.toBeVisible();
+});
+
+test("removes bookmarked entry from bookmarks page via three dots menu", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}`);
+
+  // Set a bookmark first
+  const fifthResultName = await page
+    .locator(resultsSelector)
+    .nth(4)
+    .innerText();
+  await page.locator(resultsSelector).nth(4).scrollIntoViewIfNeeded();
+  await page.locator(quickViewToggleSelector).nth(4).click();
+  await page.locator(bookmarkButtonSelector).first().click();
+
+  // Navigate to the /bookmarks page
+  await page.goto(`${baseURL}/bookmarks`);
+
+  // Selector for bookmarked item name in the /bookmarks page
+  const bookmarkedItemSelector = `[data-test-id="result-link"]:has-text("${fifthResultName}")`;
+
+  // Verify that the bookmarked item appears on the /bookmarks page
+  await expect(page.locator(bookmarkedItemSelector)).toBeVisible();
+
+  // Remove bookmark via three dots menu
+  await page.locator(xPathThreeDots).first().click();
+  await page.getByText("Remove Bookmark").click();
+
+  // Verify Message
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageRemoved)),
+  ).toBeVisible();
+  await page.locator(xPathCloseAlert).click();
+  await expect(
+    page.locator(xPathAlert.replace("{MESSAGE}", messageRemoved)),
+  ).toHaveCount(0);
+
+  // Verify the bookmarked item disappeared
+  await expect(page.locator(bookmarkedItemSelector)).not.toBeVisible();
 });
--- frontend/tests/search.spec.js
+++ frontend/tests/search.spec.js
@@ -1,5 +1,14 @@
 import { test, expect } from "@playwright/test";
 
+const resultsSelector = '[data-test-id="result-link"]';
+const xPathDropdownFilter =
+  "xpath=//button[contains(@id, 'dropdown-filters-checkboxes') and text()='{TEXT}']";
+const processedSelector = '[id="checkbox-Processed Data"]';
+const xPathProcessingStatus =
+  "xpath=//a[contains(@data-test-id, 'result-link')]/../div/div/span[text()='{STATUS}']";
+const xPathThreeDots =
+  "xpath=//a[contains(@data-test-id, 'result-link')]/../../../div[contains(@class,'mt-1 dropdown')]/div";
+
 test("search query updates URL exactly to baseURL/?q=<query>", async ({
   page,
   baseURL,
@@ -50,4 +59,124 @@ test("clicking on the quick view icon shows quick view dropdown", async ({
 
   const quickView = page.locator(quickViewSelector);
   await expect(quickView).toBeVisible();
+});
+
+test("go to details via quick view", async ({ page, baseURL }) => {
+  await page.goto(baseURL);
+
+  const quickViewTogglerSelector = '[data-test-id="quick-view-toggle-button"]';
+  const quickViewSelector = '[data-test-id="quick-view"]';
+
+  await page.locator(quickViewTogglerSelector).first().click();
+
+  const quickView = page.locator(quickViewSelector);
+  await expect(quickView).toBeVisible();
+  await page.getByText("Details").click();
+  await expect(page.getByText("Data Science Info")).toBeVisible();
+});
+
+test("get schema via quick view", async ({ page, baseURL }) => {
+  await page.goto(baseURL);
+
+  const quickViewTogglerSelector = '[data-test-id="quick-view-toggle-button"]';
+  const quickViewSelector = '[data-test-id="quick-view"]';
+
+  await page.locator(quickViewTogglerSelector).first().click();
+
+  const quickView = page.locator(quickViewSelector);
+  await expect(quickView).toBeVisible();
+
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByText("Schema").click();
+  const download = await downloadPromise;
+});
+
+test("get report via quick view", async ({ page, baseURL }) => {
+  await page.goto(baseURL);
+
+  const quickViewTogglerSelector = '[data-test-id="quick-view-toggle-button"]';
+  const quickViewSelector = '[data-test-id="quick-view"]';
+
+  await page.locator(quickViewTogglerSelector).first().click();
+
+  const quickView = page.locator(quickViewSelector);
+  await expect(quickView).toBeVisible();
+
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByText("Report (pdf)").click();
+  const download = await downloadPromise;
+});
+
+test("get dataset via quick view", async ({ page, baseURL }) => {
+  await page.goto(baseURL);
+
+  const quickViewTogglerSelector = '[data-test-id="quick-view-toggle-button"]';
+  const quickViewSelector = '[data-test-id="quick-view"]';
+
+  await page.locator(quickViewTogglerSelector).first().click();
+
+  const quickView = page.locator(quickViewSelector);
+  await expect(quickView).toBeVisible();
+
+  const promise = page.waitForEvent("download");
+  await page.getByText("Get Dataset").click();
+  const download = await promise;
+});
+
+test("get dataset via three dot menu", async ({ page, baseURL }) => {
+  await page.goto(baseURL);
+  const resultName = await page.locator(resultsSelector).nth(0).innerText();
+  await page.locator(xPathThreeDots).nth(0).click();
+  const promise = page.waitForEvent("download");
+  await page.getByText("Get Dataset").click();
+  const popup = await promise;
+});
+
+test("get dataset from detail page", async ({ page, baseURL }) => {
+  await page.goto(baseURL);
+  await page.locator(resultsSelector).nth(0).click();
+  const promise = page.waitForEvent("download");
+  await page.getByText("Get Dataset").click();
+  const download = await promise;
+});
+
+test("search for title of the first listed asset", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(baseURL);
+
+  const firstResultName = await page
+    .locator(resultsSelector)
+    .first()
+    .innerText();
+
+  await page.fill('input[type="search"]', firstResultName);
+  await page.press('input[type="search"]', "Enter");
+  await page.waitForTimeout(1000);
+  const newFirstResultName = await page
+    .locator(resultsSelector)
+    .first()
+    .innerText();
+  expect(newFirstResultName).toBe(firstResultName);
+});
+
+test("filter for asset processing", async ({ page, baseURL }) => {
+  await page.goto(baseURL);
+  await page.getByText("Filter").click();
+  await page
+    .locator(xPathDropdownFilter.replace("{TEXT}", "Asset processing"))
+    .first()
+    .click();
+  await page.locator(processedSelector).first().click();
+  await page.waitForTimeout(2000);
+  await expect(page.getByText("assetProcessingStatus")).toBeVisible();
+  await expect(
+    page
+      .locator(xPathProcessingStatus.replace("{STATUS}", "Processed Data"))
+      .first(),
+  ).toBeVisible();
+  await expect(
+    page.locator(xPathProcessingStatus.replace("{STATUS}", "Original Data")),
+  ).toHaveCount(0);
 });
