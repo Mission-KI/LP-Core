2a9320bf7f946f3f5da7a23eca85e0cabc66faa6
NENAD-BLAGOVCANIN <nenadblagovcanin2@gmail.com>
2025-04-22 20:20:41 +0200

    created admin panel pages and layout components

--- frontend/src/modules/common/components/PrivateRoutes.jsx
+++ frontend/src/modules/common/components/PrivateRoutes.jsx
@@ -1,12 +1,17 @@
-import { Navigate, Outlet } from "react-router";
+import { Navigate, Outlet, useLocation } from "react-router";
 import { useAuth } from "../contexts/AuthContext";
 
 export const PrivateRoutes = () => {
   const { authenticated, loading } = useAuth();
+  const location = useLocation();
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  return authenticated ? <Outlet /> : <Navigate to="/auth/login" />;
+  return authenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/auth/login" state={{ from: location }} replace />
+  );
 };
